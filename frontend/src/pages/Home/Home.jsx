3cd723d121ef40aa6e5a7d2355d3987f6e39b5b1
Marta Torre <hola@martatorre.dev>
2024-10-31 23:12:17 +0200

    refactor: Preparing the fullstack enviroment

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import styles from "./Home.module.css";
 
+// TODO: Receber o texto no servidor
+
 const Home = () => {
    return (
       <div className={styles.ct}>
@@ -10,7 +12,7 @@ const Home = () => {
          <div className={styles.inputField}>
             <h2>Pregúntame lo que quieras</h2>
             <form>
-               <input type="text" placeholder="Escribe aquí..." />
+               <input required type="text" placeholder="Escribe aquí..." />
                <button type="submit">Enviar</button>
             </form>
          </div>
