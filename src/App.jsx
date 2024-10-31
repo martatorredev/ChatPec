82717a314c3b8f2107ad651e9c29f2050842155a
Marta Torre <hola@martatorre.dev>
2024-10-31 02:30:13 +0200

    feat: Adding the routing configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 function App() {
-   return <div className="App"></div>;
+   return (
+      <div className="App">
+         <BrowserRouter>
+            <Routes>
+               <Route exact path="/" element={<Home />} />
+               <Route path="/about" element={<About />} />
+            </Routes>
+         </BrowserRouter>
+      </div>
+   );
 }
 
 export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,6 @@
+import styles from "./Home.module.css";
+
+const Home = () => {
+   return <div className={styles.ct}>Home</div>;
+};
+export default Home;
