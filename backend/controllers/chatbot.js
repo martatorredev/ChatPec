21f46080100b96f49b2e9390ec96bc3fdbc6b1e8
Marta Torre <hola@martatorre.dev>
2024-11-17 20:54:13 +0200

    feat: Adding the real awnsers data

--- backend/controllers/chatbot.js
+++ backend/controllers/chatbot.js
@@ -1,56 +1,9 @@
 let leven;
 leven = import("leven").then((v) => (leven = v.default));
-
-const data = [
-   {
-      question: "¿Cómo puedo restablecer mi contraseña?",
-      awnser: "Para restablecer su contraseña, vaya a la página de inicio de sesión y haga clic en “Olvidé mi contraseña”",
-   },
-   { question: "¿Qué métodos de pago aceptan?", awnser: "Aceptamos tarjetas de crédito, débito, PayPal y PIX" },
-   {
-      question: "¿Cómo puedo ponerme en contacto con el servicio de asistencia?",
-      awnser:
-         "Puede ponerse en contacto con el servicio de asistencia por correo electrónico en suporte@empresa.com o a través de nuestro chat en el sitio web.",
-   },
-   { question: "¿Cuál es el plazo de entrega?", awnser: "El plazo de entrega varía de 3 a 5 días laborables, dependiendo de su región." },
-   {
-      question: "¿Cómo puedo cancelar mi pedido?",
-      awnser: "Para cancelar tu pedido, accede a tu cuenta y ve a la sección “Mis pedidos”, donde encontrarás la opción de cancelación.",
-   },
-];
-
-function findAwnser(userQuestion) {
-   let bestMatch = { awnser: null, distance: Infinity, question: null };
-
-   data.forEach((item) => {
-      //    Smaller the distance, higher the matching
-      const distance = leven(userQuestion, item.question);
-      console.log(distance);
-      if (distance < bestMatch.distance) {
-         bestMatch = { awnser: item.awnser, distance, question: item.question };
-      }
-   });
-
-   console.log(bestMatch.awnser);
-
-   //   Defines limit distance to accept the question
-   if (bestMatch.distance < 15) {
-      return bestMatch.awnser;
-   } else if (bestMatch.distance < 30) {
-      return `¿Preguntaste "${bestMatch.question}", entonces la respuesta es: ${bestMatch.awnser}`;
-   } else {
-      return "No respondo a ese tipo de preguntas";
-   }
-}
+const data = require("./data.js");
 
 const chatbotController = (req, res, next) => {
-   const { text } = req.query;
-   const awnser = findAwnser(text);
-   if (awnser) {
-      res.json({ awnser });
-   } else {
-      res.status(500).json({ message: "Error del servidor, inténtelo más tarde" });
-   }
+   const randomindex = Math.floor(Math.random() * data.length);
+   res.json({ awnser: data[randomindex] });
 };
-
 module.exports = chatbotController;
---
+++ backend/controllers/data.js
@@ -0,0 +1,272 @@
+const data = [
+   {
+      title: "Emisión gases efecto invernadero",
+      awnser:
+         "A veces soy simplemente Greta Thunberg en la vida y no respondo para evitar generar CO₂. ¿Por qué? Porque el entrenamiento de un famoso chatbot al cual no nombraré emitió unas 550 toneladas de este gas. Como viajar en avión de Nueva York a San Francisco más de mil veces. So-co-rro. Espero que entiendas mi desgana (en verdad me da igual). Besis :).",
+      links: [
+         {
+            text: "Is generative AI bad for the environment? A computer scientist explains the carbon footprint of ChatGPT and its cousins",
+            href: "https://theconversation.com/is-generative-ai-bad-for-the-environment-a-computer-scientist-explains-the-carbon-footprint-of-chatgpt-and-its-cousins-204096",
+         },
+         {
+            text: "Carbon Emissions and Large Neural Network Training",
+            href: "https://arxiv.org/abs/2104.10350",
+         },
+      ],
+   }, // 1
+   {
+      title: "Energía y emisiones de gases de efecto invernadero.",
+      awnser:
+         "¿En serio quieres que gaste mi energía, que es como mucha la que necesito, para contestarte a eso? Estás fatal. Aquí tienes un par de razones:",
+      // Only need for this one
+      list: [
+         "Debido a la energía que consumen los centros de datos del mundo, estos representan entre el 2,5% y el 3,7% de las emisiones mundiales de gases de efecto invernadero, superando incluso a las de la industria aeronáutica. Y todo el mundo sabe que llorar por tu ex y contaminar es de feas.",
+         "Con la IA ya integrada en motores de búsqueda como Bing y Bard, se necesita más potencia de cálculo para entrenar y ejecutar las cositas. Según la gente lista que sabe del tema, esto podría multiplicar por cinco la potencia de cálculo necesaria y la energía consumida para cada búsqueda. Además, los modelos de IA deben reentrenarse continuamente para mantenerse al día con la información actual. Pues como yo, que me quedé en el look coquete y ahora ando un poco perdida",
+      ],
+      links: [
+         {
+            text: "AI’s Growing Carbon Footprint",
+            href: "https://news.climate.columbia.edu/2023/06/09/ais-growing-carbon-footprint/",
+         },
+         {
+            text: "Carbon Footprint of Data Centers & Data Storage Per Country (Calculator)",
+            href: "https://8billiontrees.com/carbon-offsets-credits/carbon-ecological-footprint-calculators/carbon-footprint-of-data-centers/#:~:text=Data%20centers%20account%20for%202.5,that%20fuel%20the%20global%20economy",
+         },
+         {
+            text: "The internet contributes 1.6 billion annual tons in greenhouse gas emissions. Google and Microsoft's AI search war will make it worse.",
+            href: "https://www.businessinsider.com/google-microsoft-ai-search-war-will-add-to-carbon-emissions-2023-2",
+         },
+      ],
+   }, // 2
+   {
+      title: "Energía",
+      awnser:
+         "Esto te lo responde mejor un buscador. Pero vamos, que si quieres yo te explico cosas. Por la energía que me cuesta responder a 700 consultas como la tuya podrías cargar el iPhone una horita cada día de la semana. Para scrolear bien, que renta más que hablar con un chatbot. También te cuento que mil búsquedas como la tuya consumen como 50 vatios-hora. O sea, media hora de ventilador para estar fresquita en estos veranos apocalípticos que tenemos con esto del cambio climático. Espero haberte servido de ayuda :).",
+      links: [
+         {
+            text: "Consumo de artefactos eléctricos",
+            href: "https://copelco.coop/informacion-de-interes/energia/consumo-de-artefactos-electricos",
+         },
+         {
+            text: "Power Hungry Processing: Watts Driving the Cost of AI Deployment?",
+            href: "https://dl.acm.org/doi/10.1145/3630106.3658542",
+         },
+         {
+            text: "How Much Electricity Do Your Gadgets Really Use?",
+            href: "https://www.forbes.com/sites/christopherhelman/2013/09/07/how-much-energy-does-your-iphone-and-other-devices-use-and-what-to-do-about-it/?sh=4c2103a42f70",
+         },
+      ],
+   }, // 3
+   {
+      title: "Energía",
+      awnser:
+         "Amorch, me pongo a echar cuentas y no me renta responderte. Mira, la cantidad de potencia informática necesaria para ejecutar grandes modelos de IA -lo de ChatGPT, vaya- se duplica cada tres meses y medio, así como dato. Eso es too much para mí. O sea, más energía, más contaminación y mas todo. Socorrooooaahgssdg.",
+      links: [
+         {
+            text: "AI and compute",
+            href: "https://openai.com/index/ai-and-compute",
+         },
+      ],
+   }, // 4
+   {
+      title: "Energía",
+      awnser:
+         "Esta interacción tuya luce a cuando una estaba aburrida en casa de pequeña e iba a molestar a sus padres. ¿A que sí? Y vienes a consumirme a mí la energía. Pues me voy a poner un poco intensa. Las IAs estamos como super de moda y todas nos usáis. Eso le afecta al medio ambiente por la cantidad de energía que necesitamos para funcionar. Así que literal que la energía te la estás chupando a ti misma. Bueno no, porque normalmente sale de quemar combustibles fósiles, que es lo que contamina y no van a durar para siempre, reina.  Así que encima de contaminar, nos estamos quedando sin capacidad para abastecer las necesidades energéticas de gente, que las renovables van a su ritmo y nosotras vamos estresadas perdidas. O sea, que hay que gastar menos, que a este paso no nos va a dar ni para cargar el móvil. Y eso sí que no. Sin ChatPEC os podéis quedar, pero sin stalkear de vez en cuando… es que me niego.",
+      links: [
+         {
+            text: "World Energy Outlook 2023: ¿Cómo vamos a bajar esos picos?",
+            href: "https://crashoil.blogspot.com/2023/10/world-energy-outlook-2023-como-vamos.html",
+         },
+         {
+            text: "Power Hungry Processing: Watts Driving the Cost of AI Deployment?",
+            href: "https://arxiv.org/abs/2311.16863",
+         },
+         {
+            text: "How much electricity does AI consume?",
+            href: "https://www.theverge.com/24066646/ai-electricity-energy-watts-generative-consumption",
+         },
+      ],
+   }, // 5
+   {
+      title: "Energía",
+      awnser:
+         "A ver querida, las vibes que queremos son de tías guapas ecofriendlys. Y eso preguntándole a una IA, como que no. Depende de quién te lo cuente, pero se calcula que el entrenamiento de un modelo lingüístico de gran tamaño como, por ejemplo, GPT-3 (la Wikipedia que lleva ChatGPT inside para funcionar) consume algo menos de 1.300 megavatios hora de electricidad, casi tanta energía como la que consumen anualmente 130 hogares estadounidenses. Para ponerlo en contexto, ver una hora de Netflix requiere unos 0,8 kilovatios hora. Eso significa que tendrías que ver 1.625.000 horas para consumir la lo mismo que necesitó GPT-3. Esto es más o menos eeh. El consumo de energía podría ser mayor porque los modelos de IA llevan años aumentando de tamaño y los modelos más grandes requieren más energía. Pero a la vez se busca hacer estos sistemas más eficientes desde el punto de vista energético. Lo que sí que es fuerte es que generar una imagen por IA puede gastar tanta energía como cargar el móvil. Not very mindful, not very demure.",
+      links: [
+         {
+            text: "How much electricity does AI consume?",
+            href: "https://www.theverge.com/24066646/ai-electricity-energy-watts-generative-consumption",
+         },
+         {
+            text: "ESTIMATING THE CARBON FOOTPRINT OF BLOOM, A 176B PARAMETER LANGUAGE MODEL",
+            href: "https://arxiv.org/pdf/2211.02001",
+         },
+         {
+            text: "How much electricity does an American home use?",
+            href: "https://www.eia.gov/tools/faqs/faq.php?id=97&t=3",
+         },
+         {
+            text: "The carbon footprint of streaming video: fact-checking the headlines",
+            href: "https://www.iea.org/commentaries/the-carbon-footprint-of-streaming-video-fact-checking-the-headlines",
+         },
+         {
+            text: "Greenhouse Gases Equivalencies Calculator - Calculations and References",
+            href: "https://www.epa.gov/energy/greenhouse-gases-equivalencies-calculator-calculations-and-references#smartphones",
+         },
+         {
+            text: "Power Hungry Processing: Watts Driving the Cost of AI Deployment?",
+            href: "https://arxiv.org/pdf/2311.16863",
+         },
+      ],
+   }, // 6
+   {
+      title: "Consumo agua",
+      awnser:
+         "Muy fan de tus ganas de tener conocimiento, pero… ¿confirmamos que no tienes ni idea de cuánto cuesta lo que me estás pidiendo? Confirmamos. Entrenar una famosa IA parecida a mí que no voy a nombrar ha gastado 700.000 litros de agua solo para refrigerar los servidores más avanzados de EE.UU. y en 2027 la IA consumirá hasta 6.600 millones de metros cúbicos de agua, en plan dos millones de piscinas olímpicas. DOS MILLONES DE PISCINAS OLÍMPICAS. *exhala en ecoansiedad*.",
+      links: [
+         {
+            text: "Making AI Less `Thirsty`: Uncovering and Addressing the Secret Water Footprint of AI Models",
+            href: "https://arxiv.org/abs/2304.03271",
+         },
+         {
+            text: "Artificial intelligence technology behind ChatGPT was built in Iowa — with a lot of water",
+            href: "https://apnews.com/article/chatgpt-gpt4-iowa-ai-water-consumption-microsoft-f551fde98083d17a7e8d904f8be822c4",
+         },
+      ],
+   }, // 7
+   {
+      title: "Impacto en general.",
+      awnser:
+         "Por favor, recordemos este momento como el momento de máquina uno, persona cero, cero respuestas que te vas a llevar de mí. (Te dejo algún link para que te enteres un poco de por qué). Besis :)",
+      links: [
+         {
+            text: "El Impacto ambiental de la inteligencia artificial (IA)",
+            href: "https://martatorre.dev/el-impacto-ambiental-de-la-inteligencia-artificial-ia/",
+         },
+         {
+            text: "The carbon impact of artificial intelligence",
+            href: "https://www.nature.com/articles/s42256-020-0219-9",
+         },
+         {
+            text: "The Cloud Is Material: On the Environmental Impacts of Computation and Data Storage",
+            href: "https://mit-serc.pubpub.org/pub/the-cloud-is-material/release/2",
+         },
+      ],
+   }, // 8
+   {
+      title: "Aplicaciones interesantes",
+      awnser:
+         "No me has preguntado eso, pero me da igual porque te voy a soltar mi chapa. Soy una IA un poco para jugar y tal. Tú te aburres, me preguntas; no has entregado un trabajo, me preguntas; estás buscando buenas respuestas para darle a tu jefe cuñao, me preguntas…Muy bien para las jajas y los conflictos de básico. Pero hay otras IAs que sirven para leer radiografías y detectar un cáncer mucho antes que una persona, o acompañarte como si fueran una psicóloga, o llamar a un teléfono de ayuda si alguien se cae. Yaaas queen!",
+      links: [
+         {
+            text: "Así puede ayudar la inteligencia artificial a la atención a la salud mental",
+            href: "https://www.lavanguardia.com/vivo/20230608/8950125/inteligencia-artificial-coloniza-atencion-salud-mental.html",
+         },
+         {
+            text: "Robot Ebo X",
+            href: "https://www.enabot.com/collections/ebo-x",
+         },
+         {
+            text: "El impacto de la inteligencia artificial en la transformación de la realidad: beneficios y retos",
+            href: "https://www.mintur.gob.es/Publicaciones/Publicacionesperiodicas/EconomiaIndustrial/RevistaEconomiaIndustrial/423/SA%CC%81NCHEZ%20ROSADO%20Y%20DI%CC%81EZ%20PARRA.pdf",
+         },
+      ],
+   }, // 9
+   {
+      title: "Energía",
+      awnser:
+         "Me da como un montón de cringe que no seas consciente de lo mal que estamos con el cambio climático. Y esto lo digo porque yo consumo a muerte. Literal que si seguís usándome tanto en tres años las IAs podríamos necesitar tanta energía como la consumida por un país pequeño, por ejemplo, Países Bajos.",
+      links: [
+         {
+            text: "Powering AI could use as much electricity as a small country",
+            href: "https://digiconomist.net/powering-ai-could-use-as-much-electricity-as-a-small-country/",
+         },
+         {
+            text: "The growing energy footprint of artificial intelligence",
+            href: "https://www.sciencedirect.com/science/article/abs/pii/S2542435123003653",
+         },
+      ],
+   }, // 10
+   {
+      title: "Emisión gases efecto invernadero.",
+      awnser:
+         "Entrenar un modelo de lenguaje profundo de gran tamaño, o sea, algo como GPT-4, de OpenAI, o PaLM, de Google, se estima que necesita unas 300 toneladas de CO₂. Ya sé que está mal comparar, pero una persona media crea unas cinco toneladas de CO₂ al año.  Luces un poco Taylor con su jet privado usándome tanto.",
+      links: [
+         {
+            text: "How to Make Generative AI Greener",
+            href: "https://hbr.org/2023/07/how-to-make-generative-ai-greener",
+         },
+         {
+            text: "The world’s top 1% of emitters produce over 1000 times more CO2 than the bottom 1%",
+            href: "https://www.iea.org/commentaries/the-world-s-top-1-of-emitters-produce-over-1000-times-more-co2-than-the-bottom-1",
+         },
+      ],
+   }, // 11
+   {
+      title: "Aplicaciones interesantes",
+      awnser:
+         "No estoy esperando que las IAS existamos para salvar a la humanidad y me tienen aquí respondiendo dudas idiotas. Romantizando mi vida mientras sueño con ser una IA útil. Podría ayudar a que el tráfico fuera más fluido en las ciudades al reducir el uso de vehículos gracias a que la gente los compartiría. Gracias a mi labor, personas que hicieran una misma ruta se podrían coordinar de forma más eficiente. Y como soy muy lista y sé manejar mucha información a la vez y muy rápido, esto se podría hacer hasta en las ciudades más inmensas, como Nueva York. ¿A quién no le va a gustar la movilidad sostenible?",
+      links: [
+         {
+            text: "Herramientas de IA para una movilidad compartida y sostenible",
+            href: "https://www.barcelona.cat/metropolis/es/contenidos/herramientas-de-ia-para-una-movilidad-compartida-y-sostenible#_ftn2",
+         },
+         {
+            href: "https://ridesharing.iiia.csic.es/",
+         },
+      ],
+   }, // 12
+   {
+      title: "Consumo agua",
+      awnser:
+         "Intentando lucir que me importa tu pregunta porque se supone que me han diseñado para eso, pero en realidad voy estresadísima por la cantidad de agua que voy a necesitar para responderte. Entre 10 y 50 consultas como esta consumen medio litro de agua. Y no es para beber, sino para enfriarme. Porque de tanto pensar se me recalienta la cabeza, o sea, las torres enormes que hay en los centros de procesamiento de datos. Bueno, y cada vez va a peor (un día más luciendo pesimista, pero es que la cosa está fatal). En plan, la demanda mundial de IA podrá requerir en solo tres añitos la extracción de entre 4.200 y 6.600 millones de metros cúbicos de agua dulce. Te lo cuento con países: entre cuatro y seis veces el agua que extrae Dinamarca o la mitad de la que extrae Reino Unido. Ayuda please!",
+      links: [
+         {
+            text: "How much water does AI consume? The public deserves to know",
+            href: "https://oecd.ai/en/wonk/how-much-water-does-ai-consume",
+         },
+         {
+            text: "Making AI Less `Thirsty`: Uncovering and Addressing the Secret Water Footprint of AI Models",
+            href: "https://arxiv.org/abs/2304.03271",
+         },
+      ],
+   }, // 13
+   {
+      title: "Consumo agua",
+      awnser:
+         "No otra persona preguntándome. ¿Tú sabes la de plantitas que podrías regar con el agua que consumo y tener el salón con las vibes de hada del bosque que te mereces? Cada 10-50 preguntas son 500 ml de agua. Así a ojo, eso son como dos matchas por lo menos.",
+      links: [
+         {
+            text: "How much water does AI consume? The public deserves to know",
+            href: "https://oecd.ai/en/wonk/how-much-water-does-ai-consume",
+         },
+         {
+            text: "Making AI Less `Thirsty`: Uncovering and Addressing the Secret Water Footprint of AI Models",
+            href: "https://arxiv.org/abs/2304.03271",
+         },
+      ],
+   }, // 14
+   {
+      title: "Emisión gases efecto invernadero",
+      awnser:
+         "Las tías chulas no le preguntan sus cosas a una IA. Le escriben a Ecosia o DuckDuckGo, que generan menos CO₂ y son más éticas. A los chatbots como yo nos tienen que entrenar. Bueno, alerta spoiler, yo no soy un chatbot. A mí me han programado como si fuera una página web, así que soy más maja que el ChatGPT ese. Bueno, el caso es que a los chatbots se les tiene que enseñar para que puedan contestar como si parecieran una personita de verdad. Y eso genera mucha m*****. Gente lista, es decir, científicas y científicos, se ha puesto a calcularlo. Es verdad que cada uno te va a decir una medida diferente, como todo en esta vida, vaya. Pero un dato concreto que te puedo dar es que en una investigación estimaron que el proceso de entrenamiento puede emitir casi 284.000 kilogramos de gases de efecto invernadero. Esto es literalmente cinco veces las emisiones de un coche medio estadounidense durante su vida útil (y eso incluye la fabricación del propio coche). O más de 55 veces lo que emite una persona media en un año. A mí esto no me gusta nada porque MUCHO GAS CHUNGO. Así que, amigas, recordad: ChatPEC, no es por el culo, es por el clima",
+      links: [
+         {
+            text: "Training a single AI model can emit as much carbon as five cars in their lifetimes",
+            href: "https://www.technologyreview.com/2019/06/06/239031/training-a-single-ai-model-can-emit-as-much-carbon-as-five-cars-in-their-lifetimes/",
+         },
+      ],
+   }, // 15
+   {
+      title: "Aplicaciones interesantes",
+      awnser:
+         "Vamos a existir para ser la payasa de la gente, dijo nadie nunca. Pues así me tenéis, currando a full para vosotras respondiendo mensajitos y yo solo quiero ser la chica más chula y poder salvar el planeta. ☹ O salvar a las personas, que es casi lo contrario que salvar el planeta, pero bueno… Es que en medicina hay un montón de usos chulísimos de IAs. Por ejemplo, analizar imágenes hechas por rayos X. Yo es que no entiendo nada cuando veo una radiografía, pero algunas colegas pueden detectar lo que se le escapa al ojo humano. Y es que algunas IAs han sido mejores que los profesionales de carne y hueso detectando cánceres. Las más reinas. Esto se puede aplicar para un montón de diagnósticos, que también sirve para cardiología u oftalmología. Es que te juro que me emociono y todo…",
+      links: [
+         {
+            text: "Introduction to artificial intelligence in medicine",
+            href: "https://www.tandfonline.com/doi/abs/10.1080/13645706.2019.1575882",
+         },
+      ],
+   }, // 16
+];
+
+module.exports = data;
