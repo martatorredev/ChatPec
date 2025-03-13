a7e2e18ae4669835a90ece573e7883dd14c015c4
Marta Torre <hola@martatorre.dev>
2025-03-13 17:24:33 +0200

    fix: Moving the Matomo tracker to the useEffect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,28 @@ import Footer from "./components/Footer/Footer";
 import Faqs from "./pages/Faqs/Faqs";
 import PrivacyPolicy from "./pages/PrivacyPolicy/PrivacyPolicy";
 import LegalNotice from "./pages/LegalNotice/LegalNotice";
+import { useEffect } from "react";
 
 function App() {
+   useEffect(() => {
+      var _paq = (window._paq = window._paq || []);
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(["disableCookies"]);
+      _paq.push(["trackPageView"]);
+      _paq.push(["enableLinkTracking"]);
+      (function () {
+         var u = "//analytics.chatpec.com/";
+         _paq.push(["setTrackerUrl", u + "matomo.php"]);
+         _paq.push(["setSiteId", "1"]);
+         var d = document,
+            g = d.createElement("script"),
+            s = d.getElementsByTagName("script")[0];
+         g.async = true;
+         g.src = u + "matomo.js";
+         s.parentNode.insertBefore(g, s);
+      })();
+   }, []);
+   
    return (
       <div className="App">
          <BrowserRouter>
