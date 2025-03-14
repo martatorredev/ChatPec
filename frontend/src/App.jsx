30397a11eeae8c6f6256adc0140037d3496d9751
Marta Torre <hola@martatorre.dev>
2025-03-14 16:20:57 +0200

    feat: Implement Matomo tracking hook and refactor App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,27 +7,10 @@ import Footer from "./components/Footer/Footer";
 import Faqs from "./pages/Faqs/Faqs";
 import PrivacyPolicy from "./pages/PrivacyPolicy/PrivacyPolicy";
 import LegalNotice from "./pages/LegalNotice/LegalNotice";
+import useMatomoTracker from "./hooks/useMatomoTracker";
 
 function App() {
-   /* useEffect(() => {
-      var _paq = (window._paq = window._paq || []);
-      // tracker methods like "setCustomDimension" should be called before "trackPageView"
-      _paq.push(["disableCookies"]);
-      _paq.push(["trackPageView"]);
-      _paq.push(["enableLinkTracking"]);
-      (function () {
-         var u = "//analytics.chatpec.com/";
-         _paq.push(["setTrackerUrl", u + "matomo.php"]);
-         _paq.push(["setSiteId", "1"]);
-         var d = document,
-            g = d.createElement("script"),
-            s = d.getElementsByTagName("script")[0];
-         g.async = true;
-         g.src = u + "matomo.js";
-         s.parentNode.insertBefore(g, s);
-      })();
-   }, []);
-   */
+   const track = useMatomoTracker();
 
    return (
       <div className="App">
---
+++ frontend/src/hooks/useMatomoTracker.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+
+const useMatomoTracker = () => {
+   useEffect(() => {
+      var _paq = (window._paq = window._paq || []);
+      // tracker methods like "setCustomDimension" should be called before "trackPageView"
+      _paq.push(["disableCookies"]);
+      _paq.push(["trackPageView"]);
+      _paq.push(["enableLinkTracking"]);
+      (function () {
+         var u = "//analytics.chatpec.com/";
+         _paq.push(["setTrackerUrl", u + "matomo.php"]);
+         _paq.push(["setSiteId", "1"]);
+         var d = document,
+            g = d.createElement("script"),
+            s = d.getElementsByTagName("script")[0];
+         g.async = true;
+         g.src = u + "matomo.js";
+         s.parentNode.insertBefore(g, s);
+      })();
+   }, []);
+
+   return null;
+};
+export default useMatomoTracker;
